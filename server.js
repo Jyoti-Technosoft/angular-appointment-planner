6802c11b93fec48978ce339c1f3d75b51946bd1e
dipesh <dipesh_gandhi9@yahoo.com>
2021-11-19 19:48:05 +0530

    deployment fixes

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ app.use(express.static(__dirname + '/'));
 app.listen(process.env.PORT || 8181);
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('dist/appointment-planner'));
+    app.use(express.static('dist\\appointment-planner'));
     app.get('*', (req, res) => {
-      res.sendFile(path.join('dist/appointment-planner', 'index.html'));
+      res.sendFile(path.join('dist\\appointment-planner', 'index.html'));
     });
   }
